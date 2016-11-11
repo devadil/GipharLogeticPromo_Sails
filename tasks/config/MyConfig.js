7b1eb84b3497e508293fcdbb15076b2614794609
androdev <haddadadil01@hotmail.fr>
2016-11-11 12:51:33 +0100

    refrech

---
+++ tasks/config/MyConfig.js
@@ -0,0 +1,27 @@
+/**
+ * Created by HADDAD on 11/11/2016.
+ */
+
+
+module.exports.myconfig = {
+
+  /*** APP REFERENCES ***/
+
+  app_name: 'GipharLogeticPromo',
+  app_version: '1.0.0',
+  app_env: 'dev',
+
+  /*** STOCKAGE ET PROPRIETE DU FICHIER PDF PROMO ***/
+
+  uri_unix: '/home/temp/',
+  uri_win: 'C:/mytemp/' ,
+
+
+  /*** DATABASE PROPORTY ***/
+
+  host :'localhost',
+  sherma : 'public',
+  user :'postgres',
+  password : 'sherine2011'
+
+}
