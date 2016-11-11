b4427a0ba352b010ef9b5e55e0668b4ee853e288
androdev <haddadadil01@hotmail.fr>
2016-11-11 10:58:56 +0100

    generation api models

---
+++ api/controllers/PdfController.js
@@ -0,0 +1,11 @@
+/**
+ * PdfController
+ *
+ * @description :: Server-side logic for managing Pdfs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/PharmacieController.js
@@ -0,0 +1,11 @@
+/**
+ * PharmacieController
+ *
+ * @description :: Server-side logic for managing Pharmacies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
